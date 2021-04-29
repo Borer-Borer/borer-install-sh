cf21ad20319c365b0970c040bb3d6ca15cab987a
borer <borer@localhost.localdomain>
2021-04-29 08:10:43 +0800

    rebuilding site Thu Apr 29 08:10:43 AM CST 2021

--- applications-install-sh/opensuse-install/rust.sh
+++ applications-install-sh/opensuse-install/rust.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
+[[ ! $(command -v gcc) ]] && $install_command gcc || echo "gcc exist!"
 if [ ! $(command -v cargo) ]; then
     curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
 else
--- applications-install-sh/opensuse-install/vim_nice.sh
+++ applications-install-sh/opensuse-install/vim_nice.sh
@@ -6,20 +6,19 @@
 [[ ! $(command -v tmux) ]] && $install_command tmux || echo "tmux exist!"
 [[ ! $(command -v rofi) ]] && $install_command rofi || echo "rofi exist!"
 [[ ! $(command -v cmus) ]] && $install_command cmus || echo "cmus exist!"
-# [[ ! $(command -v ripgrep) ]] && $install_command ripgrep || echo "ripgrep exist!"
+[[ ! $(command -v ripgrep) ]] && $install_command ripgrep || echo "ripgrep exist!"
 
 install_oh_my_zsh(){
-    # [[ ! $(command -v zsh) ]] && $install_command zsh || echo "zsh exist!"
-    # sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    [[ ! $(command -v zsh) ]] && $install_command zsh || echo "zsh exist!"
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     chsh -s $(which zsh) $USER
     ZSH_PATH=$HOME/.oh-my-zsh
     rm $HOME/.zshrc
-    ln -s $PWD/applications-install-sh/configs/.zshrc $HOME/.zshrc
-    cd $HOME/.oh-my-zsh/plugins
-    git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_PATH/plugins/zsh-autosuggestions
-    git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_PATH/themes/spaceship-prompt" --depth=1
+    cp $PWD/applications-install-sh/configs/.zshrc $HOME/.zshrc
+    [[ ! -d $ZSH_PATH/plugins/zsh-autosuggestions ]] && git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_PATH/plugins/zsh-autosuggestions || echo "autosuggestions exist"
+    [[ ! -d $ZSH_PATH/themes/spaceship-prompt ]] && git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_PATH/themes/spaceship-prompt" --depth=1 || echo "spaceship-promp exist"
+    rm $ZSH_PATH/themes/spaceship.zsh-theme
     ln -s "$ZSH_PATH/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_PATH/themes/spaceship.zsh-theme" 
-    cd $PWD
 }
 
 # install_keynav(){
@@ -29,9 +28,8 @@ install_oh_my_zsh(){
 
 install_oh_my_zsh
 # install_keynav
-
 rm $HOME/.vimrc
-rm $HOME/.tmux
-ln -s $PWD/applications-install-sh/configs/.vimrc $HOME/.vimrc
-ln -s $PWD/applications-install-sh/configs/.tmux $HOME/.tmux
-# curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+rm $HOME/.tmux.conf
+cp $PWD/applications-install-sh/configs/.vimrc $HOME/.vimrc
+cp $PWD/applications-install-sh/configs/.tmux.conf $HOME/.tmux.conf
+curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
