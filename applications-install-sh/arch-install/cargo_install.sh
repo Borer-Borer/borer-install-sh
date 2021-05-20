37e949c1ebb2ac24bc4c55dc30b22b7ee6ed3fd5
borer <janu.yieg.borer@gmail.com>
2021-05-20 20:49:35 +0800

    rebuilding site Thu May 20 08:49:35 PM CST 2021

---
+++ applications-install-sh/arch-install/cargo_install.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+# An interactive cheatsheet tool for the command-line 
+cargo install navi
+navi repo add denisidoro/cheats
+
+# A command-line benchmarking tool.
+cargo install typerfine
+
+# A fast CSV command line toolkit written in Rust. 
+cargo install xsv
+
+# A command-line hex viewer
+cargo install hexyl
+
+# Easily and securely share files from the command line.  A fully featured Firefox Send client.
+cargo install ffsend
