0820688e76da5c5c476d320cbf20882259544aa8
borer <janu.yieg.borer@gmail.com>
2021-05-20 13:31:05 +0800

    rebuilding site Thu May 20 01:31:05 PM CST 2021

--- applications-install-sh/arch-install/base_install.sh
+++ applications-install-sh/arch-install/base_install.sh
@@ -1,4 +1,5 @@
 #/bin/bash
 #GNOME Keyring is a collection of components in GNOME that store secrets, passwords, keys, certificates and make them available to applications.
 [[ ! $(command -v gnome-keyring) ]] && $install_command
-gnome-keyring || echo "gnome-keyring exist!"
+[[ ! $(command -v cowsay) ]] && $install_command
+cowsay || echo "cowsay exist!"
