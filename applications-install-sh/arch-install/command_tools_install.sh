0858f00f6fd3b3b3c1bfaa110b4ff05372052380
borer <janu.yieg.borer@gmail.com>
2021-05-21 17:00:09 +0800

    rebuilding site Fri May 21 05:00:09 PM CST 2021

--- applications-install-sh/arch-install/command_tools_install.sh
+++ applications-install-sh/arch-install/command_tools_install.sh
@@ -14,3 +14,10 @@
 [[ ! $(command -v ncdu) ]] && $install_command ncdu || echo "ncdu exist!"
 #System restore tool for Linux. Creates filesystem snapshots using rsync+hardlinks, or BTRFS snapshots. Supports scheduled snapshots, multiple backup levels, and exclude filters. Snapshots can be restored while system is running or from Live CD/USB. 
 [[ ! $(command -v timeshift) ]] && $install_command ncdu || echo "ncdu exist!"
+
+
+[[ ! $(command -v lftp) ]] && $install_command lftp || echo "lftp exist!"
+[[ ! $(command -v mutt) ]] && $install_command mutt || echo "mutt exist!"
+[[ ! $(command -v wget) ]] && $install_command wget || echo "wget exist!"
+# Sony 的 PlayStation2, SEGA 的 DreamCast, Nintendo 的 N64，Namco 的街机
+# 原来 UNIX 用户早就有非常方便的 troff, LaTeX, SGML 等东西可以处理文档
