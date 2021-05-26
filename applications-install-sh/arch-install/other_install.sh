eaff2f15b35b3e78cbbd73a689b8f8dd7c85206a
borer <janu.yieg.borer@gmail.com>
2021-05-26 16:24:16 +0800

    rebuilding site Wed May 26 04:24:16 PM CST 2021

--- applications-install-sh/arch-install/other_install.sh
+++ applications-install-sh/arch-install/other_install.sh
@@ -2,3 +2,12 @@
 [[ ! $(command -v evince) ]] && $install_command evince || echo "evince exist!"
 
 [[ ! $(command -v audacity) ]] && $install_command audacity || echo "audacity exist!"
+
+if [ ! $(command -v unclutter) ]; then
+    add_profile="unclutter -idle 10"
+    $install_command unclutter
+    grep "$add_profile" $HOME/.profile
+    if [ $?!=0 ]; then
+        "$add_profile" >> $HOME/.profile
+    fi
+fi
