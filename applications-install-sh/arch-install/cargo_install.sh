649476c811a6b7d0551ead73d620e1b80e50348e
Borer <janu.yieg.borer@gmail.com>
2022-09-22 16:18:28 +0800

    rebuilding site Thu Sep 22 04:18:28 PM CST 2022

--- applications-install-sh/arch-install/cargo_install.sh
+++ applications-install-sh/arch-install/cargo_install.sh
@@ -25,7 +25,7 @@ cargo install broot
 
 cargo install gitui
 
-cargo install tui-rs
+# cargo install tui-rs
 
 cargo install anki
 
@@ -33,4 +33,97 @@ cargo install rustlings
 
 cargo install rome
 
+#generate a book into web
 cargo install mdbook
+
+#games
+cargo install battleship-rs
+cargo install rust-sadari-cli
+cargo install minesweep
+
+#A feature rich terminal UI file transfer and explorer with support for SCP/SFTP/FTP/S3.
+cargo install termscp
+
+#Spotify for the terminal written in Rust
+cargo install spotify-tui
+
+#terminal bandwidth utilization tool
+cargo install bandwhich
+
+#Linux Kernel Manager and Activity Monitor
+cargo install kmon
+
+#Manage your GnuPG keys with easel
+cargo install gpg-tui
+
+#A TUI system monitor written in Rust
+cargo install ytop
+
+#sort of like top or htop but with zoom-able charts CPU.GPU,network,and disk usage
+cargo install zenith
+
+#Yet another cross-platform graphical process/system monitor.(btm)
+cargo install bottom
+
+#Blazing fast terminal-ui for git written in rust
+cargo install gitui
+
+#Trustworthy, encrypted, command-line TOTP/HOTP authenticator app with import functionality. 
+cargo install cotp
+
+#a network diagnostic tool(trip)
+cargo install trippy
+
+#A simple tui to view & control docker containers
+cargo install oxker
+
+#Debugger for Sed: demystify and debug your sed scripts, from comfort of your terminal. 
+cargo install desed
+
+#Realtime ticker data in your terminal
+cargo install tickrs
+
+#time management tui in rust
+cargo install rusty-krab-manager
+
+#taskwarrior-tui:A terminal user interface for taskwarrior
+cargo install taskwarrior-tui
+
+#Ping,but with a graph
+cargo install gping
+
+#A simple and fast dashboard for Kubernetes
+cargo install kdash
+
+#Terminal chat through the LAN with video streaming and file transfer.
+cargo install termchat
+#twitch chat tui 
+cargo install twitch-tui
+
+# weather tui
+cargo install weather-tui
+
+#A hackable minimal fast tui file explorer
+cargo install xplr
+#ranger-like terminal file manager written in rust
+cargo install joshuto
+#Terminal disk space navigator
+cargo install diskonaut
+
+#$Rust ADS-B decoder +tui radar application:adsb_deku
+
+
+#cli command organizer written in rust
+cargo install hoard-rs
+
+#A modern alternative to jthe watch command
+cargo install hwatch
+
+#Youtube client in terminal for music
+cargo install ytui-music
+
+#Tui image viewer 
+cargo install picterm
+
+#A cross-platform tui database management tool written in rust
+cargo instal gobang
